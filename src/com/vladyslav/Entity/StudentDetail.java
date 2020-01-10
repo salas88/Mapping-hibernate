158625860cb05200d1f9ac52396f356100bba944
Vladislav Hasporian <vladyslav.cartel@gmail.com>
2020-01-10 13:37:39 +0200

    one-to-one mapping

---
+++ src/com/vladyslav/Entity/StudentDetail.java
@@ -0,0 +1,63 @@
+package com.vladyslav.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="student_detail")
+public class StudentDetail {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="city")
+	private String city;
+	
+	@Column(name="training_shema")
+	private String trainingShema;
+	
+	public StudentDetail() {}
+
+	public StudentDetail(String city, String trainingShema) {
+		this.city = city;
+		this.trainingShema = trainingShema;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getTrainingShema() {
+		return trainingShema;
+	}
+
+	public void setTrainingShema(String trainingShema) {
+		this.trainingShema = trainingShema;
+	}
+
+	@Override
+	public String toString() {
+		return "StudentDetail [id=" + id + ", city=" + city + ", trainingShema=" + trainingShema + "]";
+	}
+
+	
+
+}
---
+++ src/com/vladyslav/Main/Solution.java
@@ -0,0 +1,49 @@
+package com.vladyslav.Main;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.vladyslav.Entity.Student;
+import com.vladyslav.Entity.StudentDetail;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+		SessionFactory factory = new Configuration()
+				 .configure("hibernate.cfg.xml")
+				 .addAnnotatedClass(Student.class)
+				 .addAnnotatedClass(StudentDetail.class)
+				 .buildSessionFactory();
+
+		Session session = factory.getCurrentSession();
+		
+		
+		try {
+			// Create object 
+			Student student = new Student("Gaga", "Kiknadze", "gaga@gmai;.com");
+			StudentDetail studentInfo = new StudentDetail("Kharkov", "daily");
+			
+			// associate objects together
+			student.setStudentDetail(studentInfo);
+			
+			// start transaction
+			session.beginTransaction();
+			
+			// save the object student
+			// because of CascadeType.All this will also save student info 
+			// object
+			session.save(student);
+			
+			session.getTransaction().commit();
+			System.out.println("Done!");
+		}finally {
+			
+			session.close();
+			
+		}
+
+	}
+
+}
