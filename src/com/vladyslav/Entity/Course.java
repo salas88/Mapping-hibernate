b6fd49bf3cfa81230fd0362eb43babbc16bcd0ee
Vladislav Hasporian <vladyslav.cartel@gmail.com>
2020-01-11 16:32:05 +0200

    add new table 'Review' for the course

--- src/com/vladyslav/Entity/Course.java
+++ src/com/vladyslav/Entity/Course.java
@@ -1,13 +1,18 @@
 package com.vladyslav.Entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -27,6 +32,31 @@ public class Course {
 	@JoinColumn(name="student_id")
 	private Student student;
 	
+	@OneToMany(fetch=FetchType.LAZY, cascade=CascadeType.ALL)
+	@JoinColumn(name="id_course")
+	private List<Review> reviews;
+	
+	public void addReview(Review tempReview) {
+		if(reviews == null) {
+		reviews = new ArrayList<>();}
+		
+		reviews.add(tempReview);
+	}
+	
+	
+	
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
+
+
 	public Course() {}
 
 	public Course(String title) {
--- src/com/vladyslav/Main/AddReviewForCourse.java
+++ src/com/vladyslav/Main/AddReviewForCourse.java
@@ -8,10 +8,11 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import com.vladyslav.Entity.Course;
+import com.vladyslav.Entity.Review;
 import com.vladyslav.Entity.Student;
 import com.vladyslav.Entity.StudentDetail;
 
-public class AddCourseForStudent {
+public class AddReviewForCourse {
 
 	public static void main(String[] args) {
 		
@@ -20,6 +21,7 @@ public class AddCourseForStudent {
 				 .addAnnotatedClass(Student.class)
 				 .addAnnotatedClass(StudentDetail.class)
 				 .addAnnotatedClass(Course.class)
+				 .addAnnotatedClass(Review.class)
 				 .buildSessionFactory();
 
 		Session session = factory.getCurrentSession();
@@ -29,24 +31,20 @@ public class AddCourseForStudent {
 			
 			// start transaction
 			session.beginTransaction();
-			int id = 3;
-			Student student = session.get(Student.class, id);
-			
-			Course course = new Course("Html and Css course");
-			Course course2 = new Course("Basic Sprng and Hibernate");
 		
-			student.add(course);
-			student.add(course2);
-	
+			Course course = new Course("SQL language0");
+			
+			course.addReview(new Review("It's amazing course "));
+			course.addReview(new Review("Thank you very much!"));
+			
 			session.save(course);
-			session.save(course2);
 			
 			session.getTransaction().commit();
 			System.out.println("Done!");
 		}finally {
 			
 			session.close();
-			
+			factory.close();
 		}
 	}
 }
